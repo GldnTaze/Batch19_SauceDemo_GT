84c3c4cb03c0693ea7e4deb0ea5de57ef2d0d3c3
Tazeoglu <guldentaze2706@gmail.com>
2025-08-22 13:07:39 +0200

    First commit

---
+++ src/test/java/com/sauceDemo/stepDefs/Hooks.java
@@ -0,0 +1,30 @@
+package com.sauceDemo.stepDefs;
+
+import com.sauceDemo.utilitis.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before
+    public  void beforMethod(){
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @After
+    public void afterMethod(Scenario scenario){
+        if (scenario.isFailed()){
+         final    byte[] screenshotAs = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+         scenario.attach(screenshotAs,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+
+    }
+}
